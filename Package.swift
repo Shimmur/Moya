097aa05f1b5c45eee56c213f0f2bed815199bc00
hassan <hassanvfx@gmail.com>
2019-11-04 12:20:19 -0800

    Removed Reactive Dependencies
    Experimental Package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,11 @@ import PackageDescription
 let package = Package(
     name: "Moya",
     products: [
-        .library(name: "Moya", targets: ["Moya"]),
-        .library(name: "ReactiveMoya", targets: ["ReactiveMoya"]),
-        .library(name: "RxMoya", targets: ["RxMoya"])
+        .library(name: "Moya", targets: ["Moya"])
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: ("5.0.0"))),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0"))
     ],
     targets: [
          .target(
@@ -23,24 +19,8 @@ let package = Package(
             exclude: [
                 "Tests",
                 "Sources/Supporting Files",
-                "Examples"]),
-        .target(
-            name: "ReactiveMoya",
-            dependencies: [
-                "Moya",
-                "ReactiveSwift"],
-            exclude: [
-                "Tests",
-                "Sources/Supporting Files",
-                "Examples"]),
-        .target(
-            name: "RxMoya",
-            dependencies: [
-                "Moya",
-                "RxSwift"],
-            exclude: [
-                "Tests",
-                "Sources/Supporting Files",
+                "Sources/RxMoya",
+                "Sources/ReactiveMoya",
                 "Examples"])
     ]
 )
